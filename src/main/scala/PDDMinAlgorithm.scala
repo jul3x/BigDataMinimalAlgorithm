ee8860da49e50009bd668714c8484cb51b61a6fe
jul3x <jprolejko@gmail.com>
2020-05-27 17:54:30 +0200

    Labels work for 2D case

--- src/main/scala/PDDMinAlgorithm.scala
+++ src/main/scala/PDDMinAlgorithm.scala
@@ -1,7 +1,19 @@
 /* PDDMinAlgorithm.scala */
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession}
 
-object PDDMinAlgorithm {
+import scala.math
+
+object PDDMinAlgorithm extends Serializable {
+  def toBinary(l: Long, length: Int): String = {
+     val value = l.toBinaryString
+     "0" * (length - value.length()) + value
+  }
+
+  def getBinaryLength(l: Long): Int = {
+    var log2 = (x: Double) => math.log10(x)/math.log10(2.0)
+    math.ceil(log2(l)).toInt
+  }
+
   def main(args: Array[String]) {
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
 
@@ -12,7 +24,18 @@ object PDDMinAlgorithm {
       .load("small_dataset.csv")
       .na.drop().cache
 
-    in_file.collect().mkString("\n");
+    val number_of_elements = in_file.count()
+    val binary_length = getBinaryLength(number_of_elements)
+    println("Number of elements! : " + number_of_elements)
+
+    val sorted_by_x = in_file.orderBy("x").rdd.zipWithIndex()
+      .map(i => (toBinary(i._2, binary_length), i._1.get(0), i._1.get(1), i._1.get(2))).cache
+
+    val sorted_by_y = sorted_by_x.sortBy(_._4.toString.toInt).zipWithIndex()
+      .map(i => (i._1._1 + toBinary(i._2, binary_length), i._1._2, i._1._3, i._1._4)).cache
+
+
+    sorted_by_y.collect().foreach(i => println(i))
 
 
     spark.stop()
